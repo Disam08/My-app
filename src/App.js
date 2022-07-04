725c6202d6d0d334a9709654dd251e9e185ec21e
Dima Samusenko <108145097+Disam08@users.noreply.github.com>
2022-07-04 22:11:00 +0300

    work2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 
 function App() {
   return (
       <div className="App" >
-
           <firstComponent text={'First'}/>
           <firstComponent text={'Second'}/>
           <div className={'animated'}>Animated</div>
